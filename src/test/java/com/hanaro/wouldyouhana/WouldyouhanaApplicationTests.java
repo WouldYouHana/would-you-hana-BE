97c564c89755a241e5612f9df653383fe1f43399
ddingorang <sang3167@gmail.com>
2024-10-21 09:32:18 +0900

    init

---
+++ src/test/java/com/hanaro/wouldyouhana/WouldyouhanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanaro.wouldyouhana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WouldyouhanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
