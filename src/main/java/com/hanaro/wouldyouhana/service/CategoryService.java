cb2da6a8229f81b441b4a4ffd4d221a77647f403
yeonchae115 <jycpg3645@naver.com>
2024-10-23 10:08:51 +0900

    카테고리 조회 기능 구현

--- src/main/java/com/hanaro/wouldyouhana/service/CategoryService.java
+++ src/main/java/com/hanaro/wouldyouhana/service/CategoryService.java
@@ -5,7 +5,6 @@ import com.hanaro.wouldyouhana.dto.CategoryDTO;
 import com.hanaro.wouldyouhana.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
