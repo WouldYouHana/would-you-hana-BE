445f6382c957759d5a91bd1aa9324085c1771bc6
freamwork97 <jsy11123@naver.com>
2024-10-28 16:59:25 +0900

    고객 및 행원 회원가입 SMTP 서버 이메일 인증

---
+++ src/main/java/com/hanaro/wouldyouhana/repository/BankerRepository.java
@@ -0,0 +1,11 @@
+package com.hanaro.wouldyouhana.repository;
+
+import com.hanaro.wouldyouhana.domain.Banker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BankerRepository extends JpaRepository<Banker, Long> {
+    boolean existsByEmail(String email);
+    Optional<Banker> findByEmail(String email);
+}
---
+++ src/main/java/com/hanaro/wouldyouhana/service/EmailService.java
@@ -0,0 +1,44 @@
+package com.hanaro.wouldyouhana.service;
+
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public String sendVerificationCode(String toEmail) {
+        String code = generateVerificationCode();
+
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setTo(toEmail);
+            helper.setSubject("이메일 인증 코드");
+            helper.setText("인증 코드: " + code);
+
+            mailSender.send(message);
+        } catch (jakarta.mail.MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return code;
+    }
+
+    private String generateVerificationCode() {
+        return String.format("%06d", new Random().nextInt(999999));
+    }
+}
