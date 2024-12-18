0aab8922933f35c0ffb673d9211b307a64b4e647
codnscodns <dnstm1119@naver.com>
2024-12-18 15:24:20 +0900

    OPTIONS 접근 허용

--- src/main/java/com/hanaro/wouldyouhana/config/SecurityConfig.java
+++ src/main/java/com/hanaro/wouldyouhana/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.hanaro.wouldyouhana.forSignIn.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -38,6 +39,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS).permitAll()
                         .requestMatchers("/members/signIn").permitAll()
                         .requestMatchers("/bankers/signIn").permitAll()
                         .requestMatchers("/members/test").hasAuthority("ROLE_USER")
@@ -62,6 +64,7 @@ public class SecurityConfig {
                         .allowedOrigins("https://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
+                        .exposedHeaders("Authorization")
                         .allowCredentials(true);
             }
         };
