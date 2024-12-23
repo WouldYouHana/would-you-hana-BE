27d39530f13b3b5ff0ad98ea1fdd51f6b1320f1f
sanghyun <sang3167@gmail.com>
2024-12-23 16:24:05 +0900

    chore: controller 메서드들 위치 변경 및 minor fix

--- src/main/java/com/hanaro/wouldyouhana/controller/CustomerMyPageController.java
+++ src/main/java/com/hanaro/wouldyouhana/controller/CustomerMyPageController.java
@@ -1,14 +1,12 @@
 package com.hanaro.wouldyouhana.controller;
 
 
-import com.hanaro.wouldyouhana.domain.Question;
-import com.hanaro.wouldyouhana.dto.likesScrap.LikesResponseDTO;
 import com.hanaro.wouldyouhana.dto.myPage.CustomerInfoResponseDTO;
 import com.hanaro.wouldyouhana.dto.myPage.CustomerInfoUpdateDTO;
 import com.hanaro.wouldyouhana.dto.myPage.InterestLocationRequestDTO;
 import com.hanaro.wouldyouhana.dto.question.QnaListDTO;
-import com.hanaro.wouldyouhana.repository.QuestionRepository;
 import com.hanaro.wouldyouhana.service.CustomerMyPageService;
+import com.hanaro.wouldyouhana.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -24,7 +22,7 @@ import java.util.List;
 public class CustomerMyPageController {
 
     private final CustomerMyPageService customerMyPageService;
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
     // 일반회원 관심지역 목록 불러오기 ("my/interestList")
     @GetMapping("/interestList")
@@ -62,6 +60,13 @@ public class CustomerMyPageController {
     public ResponseEntity<String> editInfo(@RequestBody CustomerInfoUpdateDTO customerInfoUpdateDTO, @RequestParam Long customerId) {
         String result = customerMyPageService.updateCustomerInfo(customerInfoUpdateDTO, customerId);
         return ResponseEntity.ok(result);
+    }
+
+    // 고객별 게시물 전체 조회
+    @GetMapping("/questions/{customerId}")
+    public ResponseEntity<List<QnaListDTO>> getAllQuestionsByCustomer(@PathVariable Long customerId) {
+        List<QnaListDTO> questionByCustomerList = questionService.getAllQuestionsByCustomerId(customerId);
+        return new ResponseEntity<>(questionByCustomerList, HttpStatus.OK);
     }
 
 }
