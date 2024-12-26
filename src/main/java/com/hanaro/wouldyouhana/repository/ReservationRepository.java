695e2418dff52984192da1a567416385226e4a7c
sanghyun <sang3167@gmail.com>
2024-12-26 17:42:18 +0900

    fix: docker-compose.yml 수정

--- src/main/java/com/hanaro/wouldyouhana/repository/ReservationRepository.java
+++ src/main/java/com/hanaro/wouldyouhana/repository/ReservationRepository.java
@@ -2,10 +2,13 @@ package com.hanaro.wouldyouhana.repository;
 
 import com.hanaro.wouldyouhana.domain.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     List<Reservation> findAllByCustomerId(Long customerId);
     List<Reservation> findAllByBankerName(String bankerName);
+    @Query("SELECT r FROM Reservation r WHERE r.bankerName == null AND r.branchName = :branchName")
+    List<Reservation> findAllByBranchAndNoBanker(String branchName);
 }
