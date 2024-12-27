86585b4c812360d25365488a9dcadd9a5d43104e
sanghyun <sang3167@gmail.com>
2024-12-27 17:26:27 +0900

    chore: PopularKeywords 엔티티 추가

---
+++ src/main/java/com/hanaro/wouldyouhana/domain/PopularKeywords.java
@@ -0,0 +1,21 @@
+package com.hanaro.wouldyouhana.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "keyword")
+public class PopularKeywords {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String keyword;
+    private String location;
+}
